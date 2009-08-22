2ca46a985702ba4e5944260c044f9e3aeb66866c
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2009-08-22 19:18:29 -0300

    ajax para calcular os totais do apontamento de horas

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -4,14 +4,4 @@ module EntriesHelper
     sunday = week_days.shift
     week_days.push sunday
   end
-
-  def localized_week_dates(base_date)
-    week_dates(base_date).map {|date| I18n.localize date}
-  end
-  
-  def week_dates(base_date)
-    result = []
-    7.times {|day_of_week| result << base_date.monday + day_of_week }
-    result
-  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@ $(function() {
   $(".placeHolder").focus(placeHolder)
   $(".placeHolder").blur(placeHolder)
   $(".datepicker").datepicker({ dayNamesMin: ['Se', 'Te', 'Qu', 'Qu', 'Se', 'Sá', 'Do'], monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'] });
+  $(".entry").blur(calcula_total_semana)
 })
 
 function placeHolder(){
@@ -12,5 +13,30 @@ function placeHolder(){
     if($(this).val() == ''){
       $(this).val($(this).attr('title'))
     }
-  }
+  }  
+}
+
+function calcula_total_semana(){
+	var coluna = '.'+$(this).attr('class').split(' ').pop();
+	var soma = 0;
+	$(coluna).each(function(){
+		if ($(this).hasClass('total_hour')) {
+			$(this).text(soma);
+		} else {
+			soma = soma + parseFloat($(this).val());
+		}
+	});
+	calcula_total_projeto($(this));
+}
+
+function calcula_total_projeto(campo){
+	var linha = '.'+campo.attr('class').split(' ')[1];
+	var soma = 0;
+	$(linha).each(function(){
+		if (campo.hasClass('total_project')) {
+			campo.text(soma);
+		} else {
+			soma = soma + parseFloat(campo.val());
+		}
+	});	
 }
