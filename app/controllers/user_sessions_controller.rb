84691921a4845b7e4860674a8ed1d987b23c1f01
Rodrigo Matheus <rodrigorizando@gmail.com>
2009-08-22 10:22:55 -0300

    entries are updated via ajax

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,21 @@
 class UserSessionsController < InheritedResources::Base
   skip_before_filter :require_user, :only => [:new, :create]
-  
+
+  def new
+    @user_session = UserSession.new
+    render :layout => false
+  end
+
+  def create
+    @user_session = UserSession.new params[:user_session]
+    if @user_session.save
+      flash[:notice] = I18n.t :login_successful
+      redirect_back_or_default root_path
+    else
+      render :action => :new
+    end
+  end
+
   def destroy
     current_user_session.destroy
     flash[:notice] = I18n.t :logout_successful
