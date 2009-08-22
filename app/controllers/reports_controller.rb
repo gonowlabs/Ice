c1187883ae2485845245cbb5ef99d3d17cf52afe
Diego Carrion <dc.rec1@gmail.com>
2009-08-22 06:19:32 -0300

    report controller returns cost by project

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,10 @@ class ReportsController < InheritedResources::Base
   before_filter :require_admin
   
   def cost_by_contract
-    @entries = Entry.cost_by_contract params[:contract_id], params
+    @entries = Entry.cost_by_contract params[:id], params
+  end
+  
+  def cost_by_project
+    @entries = Entry.cost_by_project params[:id], params
   end
 end
--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -8,13 +8,18 @@ describe ReportsController do
   
   context "on reports" do
     before :each do
+      @id = "34"
       @begin_date, @finish_date = Date.today - 20, Date.today - 5
     end
     
-    it "should look for the cost for a contract in a period" do
-      contract_id = "4"
-      Entry.should_receive(:cost_by_contract).with(contract_id, hash_including(:from => @begin_date, :to => @finish_date))
-      get :cost_by_contract, :contract_id => contract_id, :from => @begin_date, :to => @finish_date
+    it "should look for the cost of a contract in a period" do
+      Entry.should_receive(:cost_by_contract).with(@id, hash_including(:from => @begin_date, :to => @finish_date))
+      get :cost_by_contract, :id => @id, :from => @begin_date, :to => @finish_date
+    end
+    
+    it "should look for the cost of a project in a period" do
+      Entry.should_receive(:cost_by_project).with(@id, hash_including(:from => @begin_date, :to => @finish_date))
+      get :cost_by_project, :id => @id, :from => @begin_date, :to => @finish_date
     end
   end
 end
