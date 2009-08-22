b1c92928b9b1b10d780d95e990f8cfd2f2302aef
Diego Carrion <dc.rec1@gmail.com>
2009-08-22 09:26:59 -0300

    entries controller acts like a resource on update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,13 +1,4 @@
 class EntriesController < InheritedResources::Base
   respond_to :html, :xml, :json
   actions :index, :update
-
-  def update
-    params[:entries].each do |entry_id, hours|
-      entry = Entry.find(entry_id)
-      entry.hours = hours
-      entry.save!
-    end
-    redirect_to root_path
-  end
 end
