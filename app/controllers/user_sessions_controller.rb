2638dfe1551fe65b479a06db11a884f245e1eb8d
Diego Carrion <dc.rec1@gmail.com>
2009-08-22 19:50:12 -0300

    added missing user sessions controller spec

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < InheritedResources::Base
-  skip_before_filter :require_user, :only => [:new, :create]
+  skip_before_filter :require_user
 
   def new
     @user_session = UserSession.new
